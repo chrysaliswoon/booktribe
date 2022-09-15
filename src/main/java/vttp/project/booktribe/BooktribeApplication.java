a6fcf4dd9bc28d1c68d6f9f6a510a38434259da8
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-09-15 13:56:57 +0800

    first commit

---
+++ src/main/java/vttp/project/booktribe/BooktribeApplication.java
@@ -0,0 +1,13 @@
+package vttp.project.booktribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/project/booktribe/BooktribeApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.project.booktribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
