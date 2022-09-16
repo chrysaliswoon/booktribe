8072af48d7b2be470daab8e2c67f4941f022a23d
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-09-16 13:53:24 +0800

    Updated registration form

--- src/main/java/vttp/project/booktribe/controller/UserRestController.java
+++ src/main/java/vttp/project/booktribe/controller/UserRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,8 @@ public class UserRestController {
 
     //? CREATE new user data
     @PostMapping
-    public User createUser(@RequestBody User user) {
+    public User createUser(@RequestBody User user, Model model) {
+
         return userRepo.createUser(user);
     }
 
